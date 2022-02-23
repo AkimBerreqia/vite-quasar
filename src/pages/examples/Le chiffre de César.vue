01df4084af5df50efcabd85a8c3b1c0521c84de2
AkimBerreqia <berreqia.akim@gmail.com>
2022-02-23 16:28:18 +0000

    première page

---
+++ src/pages/examples/Le chiffre de César.vue
@@ -0,0 +1,8 @@
+<template>
+  <q-page class="pa-4 text-center">
+    <h1>Le chiffre de César</h1>
+    <p>Le premier code secret que l’on va aborder est le chiffre de César. 
+    En cryptographie, un chiffre est en réalité un synonyme de code secret. 
+    Il s’agit d’une méthode de chiffrement qui permet de transformer un message intelligible et facilement lisible (texte en clair) en un texte chiffré inintelligible.</p>
+  </q-page>
+</template>
