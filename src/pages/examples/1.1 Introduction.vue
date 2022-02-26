695c04b8e173627ca8fa255463cbc7c10f3ecf5e
AkimBerreqia <berreqia.akim@gmail.com>
2022-02-26 16:18:53 +0000

    Première page

---
+++ src/pages/examples/1.1 Introduction.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="pa-4 text-center">
+    <h1>1.1 Introduction</h1>
+
+    <p>La cryptologie est la science des codes secrets et utlise des techniques propres à l’informatique et aux mathématiques. 
+    Dans ce cours, nous n’aborderons pas les aspects mathématiques des techniques de cryptographie modernes. 
+    La cryptologie englobe d’une part la <b>cryptographie</b> qui consiste à développer des codes secrets permettant de chiffrer des messages pour qu’ils soient incompréhensibles pour les personnes à qui ils ne sont pas destinés et la <b>cryptanalyse</b> qui consiste à <i>casser les codes secrets</i> utilisés par d’autres personnes pour tenter d’accéder aux informations chiffrées.
+    </p>
+
+    <h2>1.1.1 Cryptographie</h2>
+
+    <p>Les codes secrets sont utilisés depuis l’invention de l’écriture pour communiquer des informations de manière secrète et sécurisée, notamment lors des grandes campagnes militaires. Le code de César est un exemple assez simple utilisé dans l’Antiquité que nous allons aborder au cours.<br><br>
+    Plus le nombre d’informations représentées sous forme digitale a augmenté, plus les codes secrets ont gagné en importance. Depuis l’avènement des technologies de télécommunication telles que la radio ou le télégraphe, les codes secrets jouent un rôle majeur pour le commerce et sur le champ de bataille. Le commerce électronique (e-commerce) ou les réseaux sociaux ne seraient absolument pas possibles sans les avancées modernes de la cryptographie, notamment le code RSA. À chaque fois que vous vous rendez sur un réseau social ou que vous effectuez un achat en ligne, vous utilisez des techniques modernes de cryptographie pour chiffrer les communications avec le serveur, permettant d’éviter que des tiers se procurent les informations communiquées ou ne détournent le paiement effectué.
+    </p>
+  </q-page>
+</template>
+
+<style lang="scss" scoped></style>
--- src/pages/examples/1.2 Le chiffre de César.vue
+++ src/pages/examples/1.2 Le chiffre de César.vue
@@ -1,12 +1,12 @@
 <template>
   <q-page class="pa-4 text-center">
 
-    <h1>Le chiffre de César</h1>
+    <h1>1.2 Le chiffre de César</h1>
     <p>Le premier code secret que l’on va aborder est le <b>chiffre de César</b>. 
     En cryptographie, un <b>chiffre</b> est en réalité un synonyme de <b>code secret</b>. 
     Il s’agit d’une <b>méthode de chiffrement</b> qui permet de transformer un message intelligible et facilement lisible (<b>texte en clair</b>) en un <b>texte chiffré</b> inintelligible.</p>
     
-    <h2>Énigme 1</h2>
+    <h2>1.2.1 Énigme 1</h2>
     <p>Robert utilise le chiffre de César pour chiffrer le message <p style="color:red;">RENDEZVOUSAUKIOSKATROISHEURES</p> et obtient le message chiffré <p style="color:red;">SFOEFAWPVTBVLJPTLBUSPJTIFVSFT</p>. 
     Vous remarquerez que le message chiffré est écrit en majuscules, sans accent, sans espace ni ponctuation. 
     Cela permet d’une part de faciliter le chiffrement et, d’autre part, de révéler moins d’information (notamment la longueur des mots formant le message).</p>
@@ -19,7 +19,7 @@
     <h3>Short Answer</h3>
     <h4>Q-1: Sauriez-vous expliquer comment fonctionne cette méthode de chiffrement?</h4>
     <input v-model="textArea">
-    <button v-on:click="showtext">Afficher la réponse</button>
+    <q-btn @click="showtext" style="color:darkblue;">Afficher la réponse</q-btn>
     <p v-if="isClick === true" style="color:darkblue;">Il y a un décalage qui se forme pour chaque lettre entre les deux messages. Par exemple, la lettre <i>R</i> devient la lettre <i>S</i>.</p>
 
     <p>Rodolphe utilise la même méthode de chiffrement que Robert pour chiffrer un message. Voici le message chiffré qu’il envoie à Julie. Essayez de déchiffrer ce message:</p>
@@ -29,12 +29,13 @@
     <q-input v-model="answers.rodolphe" />
     <q-btn @click="returnResult">Valider</q-btn>
     
+    <!-- Finir Ex. réponse Rodolphe -->
     <div v-show="answers.rodolphe !== ''">
     <p v-if="answers.rodolphe === correctAnswer">Bonne réponse !</p>
     <p v-if="answers.rodolphe !== correctAnswer">Mauvaise réponse...</p>
     </div>
     
-    <h2>Fonctionnement</h2>
+    <h2>1.2.2 Fonctionnement</h2>
     <p>Comme vous avez sans doute pu le deviner, le chiffre de César est très simple. 
     Il suffit de décaler toutes les lettres d’un certain nombre de positions dans l’alphabet. 
     En observant le message en clair et le message chiffré de Robert</p>
@@ -75,7 +76,7 @@
 
     <!-- Si besoin, image roue César -->
 
-    <h2>Cryptosystèmes et clés de chiffrement</h2>
+    <h2>1.2.3 Cryptosystèmes et clés de chiffrement</h2>
     <p>Il existe en réalité 25 chiffres de César différents correspondant aux 25 décalages possibles dans l’alphabet. 
     Ces différents chiffres forment ce que l’on appelle un <b>cryptosystème</b>. 
     Un cryptosystème est un ensemble de chiffres qui fonctionnent tous de la même manière à un paramètre près. 
@@ -102,17 +103,17 @@
     Alphabet chiffré : A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
     </p><br>
 
-    <h2>Activité 1</h2>
+    <h2>1.2.4 Activité 1</h2>
     <!-- Activité-1: Reprendre la même fonction que pour la Q-1 mais en l'adaptant à l'exercice, pour une clé de CESAR(4) [chiffrement] -->
 
-    <!-- Activité-2: Reprendre la même fonction que pour la Q-1 mais en l'adaptant à l'exercice, pour une clé de CESAR(4) [déchiffrement] -->
+    <!-- 1.2.5 Activité-2: Reprendre la même fonction que pour la Q-1 mais en l'adaptant à l'exercice, pour une clé de CESAR(4) [déchiffrement] -->
 
-    <!-- Activité-3A (Implémentation du chiffrement de César): Demander aide à DONNER pour faire correcteur de code pour fonction "cesar_encrypt(text, d)"
+    <!-- 1.2.6 Activité-3A (Implémentation du chiffrement de César): Demander aide à DONNER pour faire correcteur de code pour fonction "cesar_encrypt(text, d)"
     + Afficher Indice-n si bouton-n est appuyé -->
 
-    <!-- Activité 3B (Implémentation du déchiffrement de César): Demander aide à DONNER pour faire correcteur de code pour fonction "cesar_encrypt(text, d)" -->
+    <!-- 1.2.7 Activité 3B (Implémentation du déchiffrement de César): Demander aide à DONNER pour faire correcteur de code pour fonction "cesar_encrypt(text, d)" -->
 
-    <!-- Activité 4 (Cryptanalyse): Reprendre la même fonction que pour la Q-1 mais en l'adaptant à l'exercice
+    <!-- 1.2.8 Activité 4 (Cryptanalyse): Reprendre la même fonction que pour la Q-1 mais en l'adaptant à l'exercice
     + Refaire correcteur pour vérifier fonction cryptanalyse CESAR
     + Q-4: Reprendre la même fonction que pour la Q-1 mais en l'adaptant à l'exercice -->
 
