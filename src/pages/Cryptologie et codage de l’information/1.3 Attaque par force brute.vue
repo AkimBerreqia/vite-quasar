7053d7851706c9555636b3cc49f3679a6c95a068
AkimBerreqia <berreqia.akim@gmail.com>
2022-03-01 09:43:55 +0000

    Début 1.3

---
+++ src/pages/Cryptologie et codage de l’information/1.3 Attaque par force brute.vue
@@ -0,0 +1,18 @@
+<template>
+    <q-page class="pa-4 text-center">
+        <h1>1.3 Attaque par force brute</h1>
+
+        <p>
+        Comme vous avez pu le constater dans le dernier exercice de la section <a href="https://courses.21-learning.com/runestone/books/published/doi-2gy-20-21/crypto/crypto-cesar.html#donc-crypto-cesar">Le chiffre de César</a>Le chiffre de César, il est très facile de casser le chiffre de César avec un ordinateur en essayant tour à tour chaqune des clés possibles. 
+        Dans le cas du chiffre de César, très rudimentaire, il n’y a que 25 clés de chiffrement à tester (les 25 décalages possibles dans l’alphabet). 
+        Le procédé consistant à essayer bêtement toutes les clés possibles est appelé attaque par force brute (brute force attack en anglais).
+        </p>
+
+    </q-page>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped></style>
