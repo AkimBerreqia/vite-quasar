a0fa65979a4b72e217107b32318f68bb9261e8c5
AkimBerreqia <berreqia.akim@gmail.com>
2022-03-04 12:28:54 +0000

    interactif quiz

--- src/pages/tools/quiz.vue
+++ src/pages/tools/quiz.vue
@@ -1,23 +1,37 @@
 <template>
   <q-page class="pa-4 text-center">
     <h1>Exemple de quiz</h1>
-    <div v-for="(question, index) in myQuestions" :key="question.id">
+    <div v-show="index === counter" v-for="(question, index) in myQuestions" :key="question.id">
         <h2>Question numéro {{index + 1}}</h2>
         <pre v-if="false">{{question}}</pre>
         <p class="question">
         {{question.question}}
         </p>
         <div class="q-pa-md">
-            <div class="q-gutter-sm">
-                <q-radio v-for="(value, key) in question.answers" v-model="answers[index]" :key="key" :val="value" :label="value" />
+          <div v-for="(value, key) in question.answers" class="q-gutter-sm">
+            <q-radio v-model="answers[index]" :key="key" :val="value" :label="value"/>
+          </div>
+          
+          <div v-show="isClick === true">
+            <div v-if="myQuestions[index].correctAnswer === answers[index]">
+              <p class="q-px-sm" style="color:green;">Bravo</p>
             </div>
 
-            <div class="q-px-sm">
-            Your selection is: <strong>{{ answers[index] }}</strong>
+            <div v-else>
+              <p class="q-px-sm" style="color:red;">Dommage</p>
             </div>
+          </div>
+
+            
         </div>
     </div>
-    <pre>{{myQuestions}}</pre>
+    <q-btn-group>
+      <q-btn :disable="counter === 0" label="Précédent" @click="previous()"/>
+      <q-btn :disable="counter === 1" label="Suivant" @click="next()"/>
+      <q-btn v-if="counter > 0" label="Recommencer" @click="restart()"/>
+      <q-btn label="Valider" @click="changeClick()"/>
+    </q-btn-group>
+    <pre>{{myQuestions.question}}</pre>
   </q-page>
 </template>
 
@@ -26,7 +40,7 @@ import { ref, reactive } from "vue"
 
 const myQuestions = reactive([
   {
-      id: 0,
+      id: ref(0),
     question: "What is 10/2?",
     answers: {
       a: '3',
@@ -34,21 +48,46 @@ const myQuestions = reactive([
       c: '115',
 
     },
-    correctAnswer: 'b'
+    choice: ref(''),
+    correctAnswer: '5'
   },
   {
-      id: 1,
+      id: ref(1),
     question: "What is 30/3?",
     answers: {
       a: '3',
       b: '5',
       c: '10'
     },
-    correctAnswer: 'c',
+    choice: ref(''),
+    correctAnswer: '10'
   }
 ])
 
 const answers = reactive([])
+
+const isClick = ref(false)
+
+function changeClick() {
+  isClick.value = !isClick.value
+}
+
+const counter = ref(0)
+
+function restart() {
+  counter.value = 0
+  isClick.value = false
+}
+
+function next() {
+  counter.value++
+  isClick.value = false
+}
+
+function previous() {
+  counter.value--
+  isClick.value = false
+}
 
 </script>
 
