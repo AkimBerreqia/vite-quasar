8f58496948d6a9ca25c67ce2789cb47dca7737b3
AkimBerreqia <berreqia.akim@gmail.com>
2022-03-24 15:58:04 +0000

    prototype et vrai page faite avec json

--- src/pages/prototypes/1.1 Introduction.vue
+++ src/pages/prototypes/1.1 Introduction.vue
@@ -17,11 +17,7 @@
           <q-carousel-slide name="introduction" class="column no-wrap flex-center text-left">
             <q-icon name="key" size="28px" /><br>
 
-            <p>
-            La cryptologie est la science des codes secrets et utlise des techniques propres à l’informatique et aux mathématiques. 
-            Dans ce cours, nous n’aborderons pas les aspects mathématiques des techniques de cryptographie modernes. 
-            La cryptologie englobe d’une part la <b>cryptographie</b> qui consiste à développer des codes secrets permettant de chiffrer des messages pour qu’ils soient incompréhensibles pour les personnes à qui ils ne sont pas destinés et la <b>cryptanalyse</b> qui consiste à <i>casser les codes secrets</i> utilisés par d’autres personnes pour tenter d’accéder aux informations chiffrées.
-            </p>
+            <p v-html="contentData.presentation"></p>
           </q-carousel-slide>
         </q-carousel>
 
@@ -42,21 +38,13 @@
 
             <q-icon name="key" size="28px" /><br>
 
-            <p>
-            Les codes secrets sont utilisés depuis l’invention de l’écriture pour communiquer des informations de manière secrète et sécurisée, notamment lors des grandes campagnes militaires. 
-            Le code de César est un exemple assez simple utilisé dans l’Antiquité que nous allons aborder au cours.<br><br>
-            </p>
+            <p v-html="contentData.cryptographie1"></p>
           </q-carousel-slide>
 
           <q-carousel-slide name="cryptographie2" class="column no-wrap flex-center text-left">
             <q-icon name="key" size="28px" /><br>
 
-            <p>
-            Plus le nombre d’informations représentées sous forme digitale a augmenté, plus les codes secrets ont gagné en importance. 
-            Depuis l’avènement des technologies de télécommunication telles que la radio ou le télégraphe, les codes secrets jouent un rôle majeur pour le commerce et sur le champ de bataille. 
-            Le commerce électronique (e-commerce) ou les réseaux sociaux ne seraient absolument pas possibles sans les avancées modernes de la cryptographie, notamment le code RSA. 
-            À chaque fois que vous vous rendez sur un réseau social ou que vous effectuez un achat en ligne, vous utilisez des techniques modernes de cryptographie pour chiffrer les communications avec le serveur, permettant d’éviter que des tiers se procurent les informations communiquées ou ne détournent le paiement effectué.
-            </p>
+            <p v-html="contentData.cryptographie2"></p>
           </q-carousel-slide>
         </q-carousel>
 
@@ -77,36 +65,25 @@
 
             <q-icon name="key" size="28px" /><br>
 
-            <p>
-            Depuis l’avènement de la cryptographie, qui vise à communiquer ou stocker des informations sensibles de manière chiffrée pour les rendre inintelligibles, les personnes désireuses d’accéder à ces informations chiffrées ont du chercher des stratégies pour casser ces codes secrets. 
-            Comme nous le verrons, les codes secrets élaborés durant l’Antiquité sont tous relativement faciles à casser, du moins avec la puissance de calcul des ordinateurs actuels.
-            </p>
+            <p v-html="contentData.cryptanalyse1"></p>
           </q-carousel-slide>
 
           <q-carousel-slide name="cryptanalyse2" class="column no-wrap flex-center text-center">
             <q-icon name="key" size="28px" /><br>
 
-            <p>
-            Lors de la deuxième guerre mondiale, une des raisons principales de la victoire des alliés sur la Wehrmacht est que les Anglais ont réussi à casser le code Enigma utilisé par les Allemands dans toutes leurs communications militaires.
-            </p>
+            <p v-html="contentData.cryptanalyse2"></p>
           </q-carousel-slide>
 
           <q-carousel-slide name="cryptanalyse3" class="column no-wrap flex-center text-center">
             <q-icon name="key" size="28px" /><br>
 
-            <p>
-            Les techniques de cryptographie modernes permettent de créer des codes secrets beaucoup plus difficiles à casser. 
-            C’est la raison pour laquelle toutes les grandes puissances commerciales et militaires actuelles dépensent des sommes colossales pour engager les meilleurs mathématiciens, physiciens et informaticiens du monde et financer les services secrets responsables de percer les codes secrets utilisés par les adversaires pour en tirer un avantage commercial ou militaire.
-            </p>
+            <p v-html="contentData.cryptanalyse3"></p>
           </q-carousel-slide>
 
           <q-carousel-slide name="cryptanalyse4" class="column no-wrap flex-center text-center">
             <q-icon name="key" size="28px" /><br>
 
-            <p>
-            Dans ce cours, vous allez découvrir quelques-uns des codes secrets les plus célèbres inventés dans l’Antiquité et vous allez également chercher à casser ces codes secrets à l’aide de vos compétences en Python lors de travaux pratiques. 
-            Vous pourrez ainsi renforcer les compétences de programmation avec les chaînes de caractères.
-            </p>
+            <p v-html="contentData.cryptanalyse4"></p>
           </q-carousel-slide>
         </q-carousel>
       </div>
@@ -121,7 +98,10 @@
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
+import {ref, reactive} from 'vue'
+import content from '~/json/exemple.json'
+
+const contentData = ref(content.introduction)
 
 let action = reactive({
   slide1: ref('introduction'),
