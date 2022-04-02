3356ff5728614ae63274e30b98a10223cc43c1e2
AkimBerreqia <berreqia.akim@gmail.com>
2022-04-02 12:36:46 +0000

    2. ajustement changePage (tool)

--- src/pages/tools/changePage.vue
+++ src/pages/tools/changePage.vue
@@ -1,6 +1,6 @@
 <template>
-    <q-page>
-        <q-btn><router-link to="/">Home</router-link></q-btn>
-        <q-btn><router-link to="/1.1 Introduction">Intro</router-link></q-btn>
-    </q-page> 
+  <q-page>
+    <q-btn><router-link to="/">Home</router-link></q-btn>
+    <q-btn><router-link to="/1.1 Introduction">Intro</router-link></q-btn>
+  </q-page> 
 </template>
