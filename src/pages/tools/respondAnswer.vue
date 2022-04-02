b29fa3fd4ce560d01a405bd40c22171c0d415df4
AkimBerreqia <berreqia.akim@gmail.com>
2022-04-02 12:36:01 +0000

    ajustement changePage + respondAnswer

--- src/pages/tools/respondAnswer.vue
+++ src/pages/tools/respondAnswer.vue
@@ -1,35 +1,86 @@
 <template>
-    <div class="q-pa-md">
-        <div class="q-gutter-y-md column" style="max-width: 1050px">
-          <q-input bottom-slots v-model="myQuestions.rodolphe" label="Réponse :" counter maxlength="56" :dense="dense" style='text-transform:uppercase'>
-            <template v-slot:hint>
-            <p>{{respondAnswer(myQuestions.rodolphe, myQuestions.correctAnswer1, 56)}}</p>
-            </template>
-          </q-input>
-        </div>
-    </div>
+  <q-page class="pa-4">
+    <p>
+    Rodolphe utilise la même méthode de chiffrement que Robert pour chiffrer un message. 
+    Voici le message chiffré qu’il envoie à Julie. 
+    Essayez de déchiffrer ce message:
+    </p><br>
+    
+    <p class="text-center bg-info rounded-borders">
+    KFUJOWJUFBVDJOFNBEFNBJOTPJSBWJOHUIFVSFTSFKPJOTNPJBMBHBSF
+    </p><br>
 
     <div class="q-pa-md">
       <div class="q-gutter-y-md column" style="max-width: 1050px">
-        <q-input bottom-slots v-model="myQuestions.cesar4" label="Réponse :" counter maxlength="13" :dense="dense" style='text-transform:uppercase'>
+        <q-input bottom-slots v-model="myQuestions.rodolphe" label="Réponse :" counter maxlength="56" :dense="dense" style='text-transform:uppercase'>
           <template v-slot:hint>
-          <p>{{respondAnswer(myQuestions.cesar4, myQuestions.correctAnswer2, 13)}}</p>
+            <p>{{respondAnswer(myQuestions.rodolphe, myQuestions.rodolpheCorrectAnswer, 56)}}</p>
           </template>
         </q-input>
       </div>
     </div>
+    
+    <q-btn-group unelevated rounded>
+      <q-btn label="Indices" color="accent" @click="dialogVisible.indice1 = true" />
+
+      <q-dialog v-model="dialogVisible.indice1" @hide="onHide">
+      <q-card>
+        <q-card-section class="row items-center q-pb-none">
+        <strong class="text-h6" style="color:accent;">Q-Rodolphe: Indices</strong>
+        <q-space />
+        </q-card-section>
+
+        <q-card-section style="color:accent;">
+        <ul style="color:accent;">
+          <li>Toutes les lettres se suivent sans espace.</li><br>
+          <li>Il te sera plus simple de décrypter cette phrase en te servant d'une feuille et d'un crayon.</li><br>
+          <li>Le début du texte en clair est : "<strong>JETINVITE</strong>".</li>
+        </ul>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="ok" color="primary" v-close-popup @click="dialogVisible.indice1 = false" />
+        </q-card-actions>
+      </q-card>
+      </q-dialog>
+
+      <q-btn label="AFFICHER LA RÉPONSE" color="orange" @click="dialogVisible.respond2 = true" />
+
+      <q-dialog v-model="dialogVisible.respond2" @hide="onHide">
+      <q-card>
+        <q-card-section class="row items-center q-pb-none">
+        <strong class="text-h6" style="color:orange;">Q-Rodolphe: Réponse</strong>
+        <q-space />
+        </q-card-section>
+
+        <q-card-section style="color:orange;">
+        <strong>JETINVITEAUCINEMADEMAINSOIRAVINGTHEURESREJOINSMOIALAGARE</strong>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="ok" color="primary" v-close-popup @click="dialogVisible.respond2 = false" />
+        </q-card-actions>
+      </q-card>
+      </q-dialog>
+    </q-btn-group>
+  </q-page>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
 
+let dialogVisible = reactive({
+  respond2: ref(false),
+  indice1: ref(false)
+})
+
+function onHide(order) {
+  dialogVisible.order.value = false
+}
+
+const dense = ref(false)
+
 const myQuestions = reactive({
   rodolphe: '',
-  correctAnswer1: 'JETINVITEAUCINEMADEMAINSOIRAVINGTHEURESREJOINSMOIALAGARE',
-  counterLength1: 56,
-  cesar4: '',
-  correctAnswer2: 'GVCTXSKVETLMI',
-  counterLength2: 13
+  rodolpheCorrectAnswer: 'JETINVITEAUCINEMADEMAINSOIRAVINGTHEURESREJOINSMOIALAGARE'
 })
 
 function respondAnswer(exercice, correctAnswer, maxLength){
