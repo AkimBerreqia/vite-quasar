f409b10c37993ca22c69d39caa997b008392c7b8
AkimBerreqia <berreqia.akim@gmail.com>
2022-04-02 19:51:26 +0000

    btnGroup tool

---
+++ src/pages/tools/btnGroup.vue
@@ -0,0 +1,8 @@
+<template>
+    <q-page class="pa-4">
+        <q-btn-group rounded elevated center>
+            <q-btn color="green" label="btn n° 1"></q-btn>
+            <q-btn color="blue" label="btn n° 2"></q-btn>
+        </q-btn-group>
+    </q-page>
+</template>
