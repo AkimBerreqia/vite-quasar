3684dece67f8c722214b81c0c6ec506e428ce472
AkimBerreqia <berreqia.akim@gmail.com>
2022-04-02 19:58:24 +0000

    ajustement q-btn-group

--- src/pages/tools/btnGroup.vue
+++ src/pages/tools/btnGroup.vue
@@ -1,6 +1,6 @@
 <template>
     <q-page class="pa-4">
-        <q-btn-group rounded elevated center>
+        <q-btn-group rounded elevated>
             <q-btn color="green" label="btn n° 1"></q-btn>
             <q-btn color="blue" label="btn n° 2"></q-btn>
         </q-btn-group>
