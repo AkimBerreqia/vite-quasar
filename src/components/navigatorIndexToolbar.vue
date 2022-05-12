1f4deb8c3952edc4d33b3dcbeab6393f7350785c
AkimBerreqia <berreqia.akim@gmail.com>
2022-05-12 15:31:46 +0000

    navigatorBtn correction (navigatorToolbar)

--- src/components/navigatorIndexToolbar.vue
+++ src/components/navigatorIndexToolbar.vue
@@ -2,9 +2,6 @@
     <q-page-sticky position="top" expand class="bg-positive text-white text-left">
         <q-toolbar-title>1.0 Index</q-toolbar-title>
         
-        <span>
-            <q-btn icon="home" to="/"></q-btn>
-            <q-btn icon="keyboard_arrow_right" to="/1.1 Introduction">1.1 Introduction</q-btn>
-        </span>
+        <span><q-btn icon="keyboard_arrow_right" to="/1.1 Introduction">1.1 Introduction</q-btn></span>
     </q-page-sticky>
 </template>
