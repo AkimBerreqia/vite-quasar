d52949cfda11c5cec733dbdc7839863466af23f5
AkimBerreqia <berreqia.akim@gmail.com>
2022-05-13 16:49:51 +0000

    oral exemple (Hello World ! appears)

---
+++ src/pages/oralExemple.vue
@@ -0,0 +1,34 @@
+<template>
+    <q-page>
+        <div class="text-center" style="font-weight: bold;">
+            <q-btn
+            unelevated
+            Rounded
+            label="Afficher du texte"
+            color="green"
+            @click="isVisible"
+            /><br><br>
+
+            <p v-show="visible === true">Hello World !</p>
+        </div>
+    </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const visible = ref(false)
+
+function isVisible() {
+  visible.value = !visible.value
+}
+</script>
+
+<style lang="scss">
+
+
+p {
+    font-family: 'Times New Roman', Times, serif;
+}
+</style>
+
