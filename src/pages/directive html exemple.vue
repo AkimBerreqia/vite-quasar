0f6587fd048691c7c5444796aece96c9440a650f
AkimBerreqia <berreqia.akim@gmail.com>
2022-05-15 08:36:36 +0000

    v-html exemple pour oral

---
+++ src/pages/directive html exemple.vue
@@ -0,0 +1,14 @@
+<template>
+    <q-page>
+        <p v-html="text"></p><br>
+        <p> {{ text }} </p>
+    </q-page>
+</template>
+
+<script setup lang="ts">
+const text = '\
+<p>\
+<span style="color:red; font-weight:bold;">Hello </span>\
+<span style="color:blue; font-style:italic;">World !</span>\
+</p>'
+</script>
